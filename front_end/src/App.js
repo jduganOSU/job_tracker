71c491e12845e9c92cecbb2819f13a55f4a788f5
Luis <008luisf@gmail.com>
2024-04-25 01:20:00 -0700

    add HomePage

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage'; // Import HomePage component
 import Login from './components/Login';
 import CreateAccount from './components/CreateAccount';
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Login />} />
+        <Route exact path="/" element={<HomePage />} /> {/* Use HomePage for the "/" route */}
+        <Route path="/login" element={<Login />} /> {/* Route for the login page */}
         <Route path="/create-account" element={<CreateAccount />} />
       </Routes>
     </Router>
