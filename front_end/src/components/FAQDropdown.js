b7cef549299ad230b368dadba0523d34e4301c43
Luis <008luisf@gmail.com>
2024-04-25 01:21:38 -0700

    Add drowndown menu for homepage

---
+++ front_end/src/components/FAQDropdown.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './FAQDropdown.scss'; // Import Sass file
+
+function FAQDropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="accordion">
+      <div className="card">
+        <div className="card-header" onClick={handleToggle}>
+          <h2>What is Career Pilot?</h2>
+        </div>
+        {isOpen && (
+          <div className="card-body">
+            <p>Career Pilot is a platform designed to help you track the jobs you've applied to. It allows you to manage your job applications, keep track of your progress, and stay organized throughout your job search journey.</p>
+          </div>
+        )}
+      </div>
+      <div className="card">
+        <div className="card-header" onClick={handleToggle}>
+          <h2>How do you use Career Pilot?</h2>
+        </div>
+        {isOpen && (
+          <div className="card-body">
+            <p>Using Career Pilot is a breeze! Begin by signing up, then effortlessly input your job details and status. Keep everything up-to-date with ease, ensuring your job search journey remains smooth and organized.</p>
+          </div>
+        )}
+      </div>
+      <div className="card">
+        <div className="card-header" onClick={handleToggle}>
+          <h2>Is Career Pilot free?</h2>
+        </div>
+        {isOpen && (
+          <div className="card-body">
+            <p>Absolutely! Career Pilot is completely free to use, empowering you to streamline your job search journey without any cost.</p>
+          </div>
+        )}
+      </div>
+      <div className="card">
+        <div className="card-header" onClick={handleToggle}>
+          <h2>Who can use Career Pilot?</h2>
+        </div>
+        {isOpen && (
+          <div className="card-body">
+            <p> Career Pilot is for everyone embarking on their job search journey, whether you're a recent graduate taking your first steps into the professional world or an experienced professional seeking new opportunities.</p>
+          </div>
+        )}
+      </div>
+    </div>
+    
+  );
+}
+
+export default FAQDropdown;
