90f2a9f5c7cd66802a85576ee1b33f6d776a8043
Luis <008luisf@gmail.com>
2024-05-01 00:34:21 -0700

    Add CreateJob route

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage'; // Import HomePage component
 import Login from './components/Login';
 import CreateAccount from './components/CreateAccount';
+import CreateJob from './components/CreateJob';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<HomePage />} /> {/* Use HomePage for the "/" route */}
         <Route path="/login" element={<Login />} /> {/* Route for the login page */}
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/create-job" element={<CreateJob />} />
       </Routes>
     </Router>
   );
