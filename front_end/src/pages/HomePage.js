53c9e89aa7bcdc34b2c03bbbab2c68780e175d7b
Luis <008luisf@gmail.com>
2024-05-09 11:10:12 -0700

    fix missing text

--- front_end/src/pages/HomePage.js
+++ front_end/src/pages/HomePage.js
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import FAQDropdown from '../components/FAQDropdown';
 import Footer from '../components/Footer';
+import Typed from 'typed.js'; // Import Typed.js library
 import './css/HomePage.scss'; // Import Sass file
 import undrawStatsSVG from '../assets/undraw_stats.svg';
 
@@ -15,6 +16,20 @@ function HomePage() {
     navigate('/login'); // Navigate to the login page
   };
 
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Apply.', 'Track.', 'Conquer.'],
+      typeSpeed: 120,
+      backSpeed: 120,
+      loop: true,
+      backDelay: 2000,
+    });
+
+    // Cleanup
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <div className="homepage">
@@ -60,7 +75,7 @@ function HomePage() {
         <h1>Frequently Asked Questions</h1>
         <p>Our go-to resource for all inquiries. Find answers to common questions about navigating the platform, optimizing your job search, and utilizing our tracking features to their fullest.</p>
       </div>
-      {/* Include the FAQDropdown component */}
+
       <FAQDropdown />
       <Footer />
     </div>
