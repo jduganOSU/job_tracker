d283b55ddcadaceefd72d5aaf9eec28704c65958
John <duganjon@oregonstate.edu>
2024-05-20 16:51:50 -0400

    fix company unique bug

--- server/database/models/Company.js
+++ server/database/models/Company.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 const CompanySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   industry: { type: String },
   description: { type: String }, 
   location: {type: String},
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } // Reference to User model
 });
 
-CompanySchema.index({ userId: 1 });
 module.exports = mongoose.model('Company', CompanySchema);
