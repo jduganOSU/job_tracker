1f4a4022bcec788cd81fef736ab6c860e71088dc
Andrew Haylett <91613406+hayletta@users.noreply.github.com>
2024-05-22 16:46:52 -0400

    Update skillsService.js

--- server/database/services/skillsService.js
+++ server/database/services/skillsService.js
@@ -1,26 +1,26 @@
 const Skill = require('../models/Skill');
 
 const skillService = {
-    createSkill: async function(skillData) {
-        const skill = new Skill(skillData);
-        return await skill.save();
-    },
+  createSkill: async function(skillData) {
+    const skill = new Skill(skillData);
+    return await skill.save();
+  },
 
-    getAllSkillsForUser: async function(userId) {
-        return await Skill.find({ userId: userId });
-    },
+  getAllSkills: async function(userId) { // Accept userId as a parameter
+    return await Skill.find({ userId: userId }); // Filter by userId
+  },
 
-    getSkillById: async function(id) {
-        return await Skill.findById(id);
-    },
+  getSkillById: async function(id) {
+    return await Skill.findById(id);
+  },
 
-    updateSkill: async function(id, skillData) {
-        return await Skill.findByIdAndUpdate(id, skillData, { new: true });
-    },
+  updateSkill: async function(id, skillData) {
+    return await Skill.findByIdAndUpdate(id, skillData, { new: true });
+  },
 
-    deleteSkill: async function(id) {
-        return await Skill.findByIdAndDelete(id);
-    }
+  deleteSkill: async function(id) {
+    return await Skill.findByIdAndDelete(id);
+  }
 };
 
 module.exports = skillService;
