30a42468f05bf9d14ccb62dcabdc0a1d71a629ea
Andrew Haylett <91613406+hayletta@users.noreply.github.com>
2024-05-22 16:47:10 -0400

    Update contact.js

--- server/database/models/contact.js
+++ server/database/models/contact.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const contactSchema = new Schema({
-    name: { type: String, required: true },
-    company: { type: Schema.Types.ObjectId, ref: 'Company', required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true } // Ensure this matches your user reference
+const ContactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
-const Contact = mongoose.model('Contact', contactSchema);
-
-module.exports = Contact;
+module.exports = mongoose.model('Contact', ContactSchema);
